bcf47af2851779c6dc9a6b92d50f8ede64c73181
tsasselli <tsasselli@gmail.com>
2018-04-20 15:39:55 -0700

    added edit functionality to db, and updated routing

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,7 +1,8 @@
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../product.service';
+import 'rxjs/add/operator/take';
 
 @Component({
   selector: 'app-product-form',
@@ -9,19 +10,27 @@ import { ProductService } from '../../product.service';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
+  product: any = { };
   categories$;
-
+  id;
   constructor(private categoryService: CategoryService,
               private productService: ProductService,
-              private router: Router) {
+              private router: Router,
+              private route: ActivatedRoute) {
     this.categories$ = categoryService.getCategories();
+
+     this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) { this.productService.getProduct(this.id).take(1).subscribe(p => this.product = p); }
   }
 
   ngOnInit() {
   }
 
   save(product) {
+    if (this.id) { this.productService.updateProduct(this.id, product);
+  } else {
     this.productService.create(product);
+    }
     this.router.navigate(['/admin/products']);
  }
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,11 +5,20 @@ import { Injectable } from '@angular/core';
 export class ProductService {
 
   constructor(private db: AngularFireDatabase) { }
+
   create(product) {
     return this.db.list('/products').push(product);
   }
 
   getAll() {
     return this.db.list('/products');
+  }
+
+  getProduct(productId) {
+    return this.db.object('/products/' + productId);
+  }
+
+  updateProduct(productId, product) {
+    return this.db.object('/products/' + productId).update(product);
   }
 }
