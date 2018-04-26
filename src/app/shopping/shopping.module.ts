9e95bfab9abdd076d022c79eb18934186731a57e
tsasselli <tsasselli@gmail.com>
2018-04-25 20:05:10 -0700

    added shopping module

---
+++ src/app/shopping/shopping.module.ts
@@ -0,0 +1,41 @@
+import { SharedModule } from 'shared/shared.module';
+import { AuthGuardService } from 'shared/service/auth-guard.service';
+import { RouterModule } from '@angular/router';
+import { ShippingFormComponent } from './components/shipping-form/shipping-form.component';
+import { ShoppingCartSummaryComponent } from './components/shopping-cart-summary/shopping-cart-summary.component';
+import { ProductFilterComponent } from './components/products/product-filter/product-filter.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductsComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuardService] },
+      { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuardService] },
+      { path: 'order-success/:id', component: OrderSuccessComponent, canActivate: [AuthGuardService] },
+
+    ])
+  ],
+  declarations: [
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    ProductFilterComponent,
+    ShoppingCartSummaryComponent,
+    ShippingFormComponent,
+  ]
+})
+export class ShoppingModule { }
